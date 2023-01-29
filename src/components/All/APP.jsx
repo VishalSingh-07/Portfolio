858b34ffaa7aa7d22597bd587b578d3f3b6efb17
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-01-30 00:09:44 +0530

    Adding Preloader

--- src/components/All/APP.jsx
+++ src/components/All/APP.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Sidebar from "../sidebar/Sidebar"
 import Home from "../home/home"
 import About from "../about/About"
@@ -6,19 +6,34 @@ import Education from "../Education/Education"
 import Skill from "../skills/Skills.jsx"
 import Portfolio from "../portfolio/Portfolio"
 import Contact from "../contact/Contact"
+import Preloader from "../Preloader/Preloader"
 
 function APP() {
+	const [loading, setloading] = useState(false)
+
+	useEffect(() => {
+		setloading(true)
+		setInterval(() => {
+			setloading(false)
+		}, 4000)
+	}, [])
 	return (
 		<div>
-			<Sidebar />
-			<main classname="main">
-				<Home />
-				<About />
-				<Education />
-				<Skill />
-				<Portfolio />
-				<Contact />
-			</main>
+			{loading ? (
+				<Preloader loading={loading} />
+			) : (
+				<div>
+					<Sidebar />
+					<main classname="main">
+						<Home />
+						<About />
+						<Education />
+						<Skill />
+						<Portfolio />
+						<Contact />
+					</main>
+				</div>
+			)}
 		</div>
 	)
 }
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Test from "./Test"
+import "./Preloader.css"
+function Preloader(props) {
+	return (
+		<div className="main_loader">
+			<Test/>
+		</div>
+	)
+}
+
+export default Preloader
